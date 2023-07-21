c52198841b3711a993a1a25ecfd7c0143b277b65
bhattrajdev <devrajbhatt010@gmail.com>
2023-07-21 09:14:24 +0545

    modified hero

---
+++ data.jsx
@@ -0,0 +1,8 @@
+const images = [
+    {src:"/images/banner1.jpg"},
+    {src:"/images/banner2.jpg"},
+    {src:"/images/banner3.jpg"},
+    {src:"/images/banner4.jpg"},
+]
+
+export default images
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import images from '../../data';
 
 const Hero = () => {
        var settings = {
-         dots: true,
+         dots: false,
          Infinite: true,
          speed: 500,
          slidesToShow: 1,
@@ -13,20 +14,13 @@ const Hero = () => {
          autoplay: true,
        };
        return (
-           <Slider {...settings}>
-             <div>
-               <img src="/images/banner1.jpg" className='w-full h-full z-10' />
+         <Slider {...settings}>
+           {images.map((images, index) => (
+             <div key={index} className="w-full">
+               <img src={images.src} className="w-full h-full z-10" />
              </div>
-             <div>
-               <img src="/images/banner2.jpg" className='w-full h-full' />
-             </div>
-             <div>
-               <img src="/images/banner3.jpg" className='w-full h-full' />
-             </div>
-             <div>
-               <img src="/images/banner4.jpg" className='w-full h-full' />
-             </div>
-           </Slider>
+           ))}
+         </Slider>
        );
 }
 
